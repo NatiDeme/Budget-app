1e92fa5cacc3f9880337e91414ba003f201c2ac9
NatiDeme <demelashnatnael@gmail.com>
2022-10-11 17:57:16 +0300

    Add categories page with add feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'categories#index'
   devise_for :users
+
+  resources :categories, only: %i[index new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
